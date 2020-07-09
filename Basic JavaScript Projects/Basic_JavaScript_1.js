9ca15ae75f09da349fc1104ade1de18fbfc565b8
jacquelinemcquaid <67380308+jacquelinemcquaid@users.noreply.github.com>
2020-07-09 15:10:11 -0300

    Add files via upload

---
+++ Basic JavaScript Projects/Basic_JavaScript_1.js
@@ -0,0 +1,19 @@
+var A = "Welcome";
+var B = "Have a good day. ";
+var C = "Thanks for visiting this site!";
+
+var B = B.fontcolor("red");
+var C = C.fontcolor("blue");
+
+{window.alert(A);
+}
+
+{document.write("\"Act as if what you do makes a difference. It does\"-William James<br><br><br>")}
+
+{document.write("Pop Quiz: How many days are in a year?<br>")}
+
+{document.write(73*5)}
+
+{document.write("<br><br>")}
+
+{document.write(B+C)}
