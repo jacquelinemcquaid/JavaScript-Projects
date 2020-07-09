048d51a717789a69730bc8f55638ae35726cc251
jacquelinemcquaid <67380308+jacquelinemcquaid@users.noreply.github.com>
2020-07-09 17:18:11 -0300

    Add files via upload

---
+++ Basic JavaScript Projects/Basic_JavaScript_2.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    var sentence = "There are 195 countries. ";
+    sentence += "The biggest country is Russia.";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
