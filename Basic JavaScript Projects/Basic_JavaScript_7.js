cd3d45723f75cf42fefca2023f4322de4ddf5434
jacquelinemcquaid <67380308+jacquelinemcquaid@users.noreply.github.com>
2020-07-13 12:20:38 -0300

    Add files via upload

---
+++ Basic JavaScript Projects/Basic_JavaScript_7.js
@@ -0,0 +1,25 @@
+var X = 10;
+function Add_numbers_1() {
+    document.write(20 + X + "<br>");
+
+}
+
+function Add_numbers_2() {
+    document.write(X + 100 + "<br>");
+}
+
+Add_numbers_1();
+Add_numbers_2();
+
+function Add_numbers_3() {
+    var Y = 10;
+    document.write(20 + Y + "<br>");
+
+}
+
+function Add_numbers_4() {
+    document.write(Y + 100);
+}
+
+Add_numbers_3();
+Add_numbers_4()
